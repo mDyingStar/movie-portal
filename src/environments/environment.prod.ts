6e8a187c36d3ca0b1c4edfccbe9b66be93af477f
Alex <AlexMitDemBart@gmail.com>
2023-01-30 10:56:22 +0100

    use github secret for api key;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey: '<<api-key>>',
+  apiKey: process.env["API_KEY"],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: '<<api-key>>',
+  apiKey: process.env["API_KEY"],
 };
 
 /*
