b8ed0081394f53563a84ae00d0c312b6f4a32a2b
mDyingStar <alexmitdembart@gmail.com>
2023-01-30 14:40:37 +0100

    refactored package structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {MovieCardComponent} from './components/movie-card/movie-card.component';
 import {MovieListComponent} from './components/movie-list/movie-list.component';
 import {MovieHomePageComponent} from './components/movie-home-page/movie-home-page.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from "@angular/forms";
-import { MovieTrailerCardComponent } from './components/movie-trailer-card/movie-trailer-card.component';
-import { MovieTrailerListComponent } from './components/movie-trailer-list/movie-trailer-list.component';
+import {MovieTrailerListComponent} from './components/movie-trailer-list/movie-trailer-list.component';
+import {MovieCardComponent} from "./components/movie-list/movie-card/movie-card.component";
+import {
+  MovieTrailerCardComponent
+} from "./components/movie-trailer-list/movie-trailer-card/movie-trailer-card.component";
 
 @NgModule({
   declarations: [
